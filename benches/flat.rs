9478dcc0bde6dc5b459d7852d62eeb9b33cddd9d
Jesse A. Tov <jesse.tov@gmail.com>
2017-05-24 12:56:03 -0500

    Benchmarks for other two interpreters.

--- benches/flat.rs
+++ benches/flat.rs
@@ -4,28 +4,28 @@ extern crate test;
 extern crate bf;
 
 use bf::ast;
+use bf::rle_ast;
+use bf::flat;
 use bf::interpreter::Interpretable;
 use bf::test_helpers;
 
 use test::Bencher;
 
 #[bench]
-fn parse_empty_program(b: &mut Bencher) {
-    b.iter(|| {
-        ast::parse_program(b"").unwrap()
-    })
-}
+fn compile_factor(b: &mut Bencher) {
+    let program = ast::parse_program(test_helpers::FACTOR_SRC).unwrap();
 
-#[bench]
-fn parse_factor(b: &mut Bencher) {
     b.iter(|| {
-        ast::parse_program(test_helpers::FACTOR_SRC).unwrap()
-    })
+        let program = rle_ast::compile(&program);
+        flat::compile(&program)
+    });
 }
 
 #[bench]
 fn interpret_factor_million(b: &mut Bencher) {
     let program = ast::parse_program(test_helpers::FACTOR_SRC).unwrap();
+    let program = rle_ast::compile(&program);
+    let program = flat::compile(&program);
 
     b.iter(|| {
         program.interpret_memory(None, b"1000000\n").unwrap()
