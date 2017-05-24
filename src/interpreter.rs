7d43ad049870395d0d0bf9a68dd435685a92bc7e
Jesse A. Tov <jesse.tov@gmail.com>
2017-05-23 23:56:39 -0500

    Some interpreter tests.

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -38,3 +38,73 @@ fn interpret_instruction<R, W>(instruction: &Instruction, state: &mut State,
     }
 }
 
+#[cfg(test)]
+mod tests {
+    use super::interpret;
+    use super::super::ast::{Instruction, make_loop};
+    use super::super::ast::Instruction::*;
+
+    #[test]
+    fn assert_no_output() {
+        assert_interpret(&[Right], &[], &[]);
+    }
+
+    #[test]
+    fn assert_output_0() {
+        assert_interpret(&[Right, Out], &[], &[0]);
+    }
+
+    #[test]
+    fn assert_output_1() {
+        assert_interpret(&[Up, Out], &[], &[1]);
+    }
+
+    #[test]
+    fn assert_increment_input() {
+        let prog = &[In, Up, Out];
+        assert_interpret(prog, &[0], &[1]);
+        assert_interpret(prog, &[5], &[6]);
+        assert_interpret(prog, &[255], &[0]);
+    }
+
+    #[test]
+    fn assert_increment_loop() {
+        let prog = &[In, make_loop(vec![Up, Out, In])];
+        assert_interpret(prog, &[0], &[]);
+        assert_interpret(prog, &[1, 0], &[2]);
+        assert_interpret(prog, &[1, 4, 0], &[2, 5]);
+        assert_interpret(prog, &[8, 255, 18, 0], &[9, 0, 19]);
+    }
+
+    #[test]
+    fn hello_world() {
+        assert_parse_interpret("++++++[>++++++++++++<-]>.\
+                                >++++++++++[>++++++++++<-]>+.\
+                                +++++++..+++.>++++[>+++++++++++<-]>.\
+                                <+++[>----<-]>.<<<<<+++[>+++++<-]>.\
+                                >>.+++.------.--------.>>+.",
+            "",
+            "Hello, World!");
+    }
+
+    fn assert_interpret(program: &[Instruction], input: &[u8], output: &[u8]) {
+        use super::super::state::State;
+        use std::io::Cursor;
+        use std::str;
+
+        let mut reader = Cursor::new(input);
+        let mut writer = Cursor::new(Vec::<u8>::new());
+        let mut state  = State::new();
+
+        interpret(program, &mut state, &mut reader, &mut writer);
+        assert_eq!(str::from_utf8(writer.into_inner().as_slice()),
+                   str::from_utf8(output));
+    }
+
+    fn assert_parse_interpret(program: &str, input: &str, output: &str) {
+        use super::super::parser::parse_program;
+
+        let program = parse_program(program.as_bytes()).unwrap();
+        assert_interpret(&program, input.as_bytes(), output.as_bytes());
+    }
+}
