a4f454f6cb22ef41c902f00f9765bf2e6fde41df
Jesse A. Tov <jesse.tov@gmail.com>
2017-05-26 13:48:08 -0500

    Removed rle smart constructors.

--- src/rle/mod.rs
+++ src/rle/mod.rs
@@ -17,14 +17,3 @@ pub enum Instruction {
     Loop(Box<Program>),
 }
 
-pub fn mk_left(count: Count)  -> Instruction { Instruction::Op((Command::Left, count)) }
-pub fn mk_right(count: Count) -> Instruction { Instruction::Op((Command::Right, count)) }
-pub fn mk_up(count: Count)    -> Instruction { Instruction::Op((Command::Up, count)) }
-pub fn mk_down(count: Count)  -> Instruction { Instruction::Op((Command::Down, count)) }
-pub fn mk_in(count: Count)    -> Instruction { Instruction::Op((Command::In, count)) }
-pub fn mk_out(count: Count)   -> Instruction { Instruction::Op((Command::Out, count)) }
-
-/// Takes a vector of instructions and makes them into a loop.
-pub fn mk_loop(instructions: Vec<Instruction>) -> Instruction {
-    Instruction::Loop(instructions.into_boxed_slice())
-}
