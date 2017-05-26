3a7dc8b20b87e5221e51db28a6808800720f3981
Jesse A. Tov <jesse.tov@gmail.com>
2017-05-26 09:16:55 -0500

    Added assert_interpret_result for handling failure.

--- src/test_helpers.rs
+++ src/test_helpers.rs
@@ -1,5 +1,6 @@
 use std::str;
 
+use result::BfResult;
 use traits::Interpretable;
 
 pub const FACTOR_SRC: &'static [u8] = include_bytes!("../bf/factor.bf");
@@ -12,9 +13,16 @@ pub const HELLO_WORLD_SRC: &'static [u8] =
       >>.+++.------.--------.>>+.";
 
 pub fn assert_interpret<I: Interpretable + ?Sized>(program: &I, input: &[u8], output: &[u8]) {
-    let actual_bytes = program.interpret_memory(None, input).unwrap();
-    let actual = str::from_utf8(&actual_bytes).unwrap();
-    let expected = str::from_utf8(output).unwrap();
+    assert_interpret_result(program, input, Ok(output));
+}
+
+pub fn assert_interpret_result<I>(program: &I, input: &[u8], output: BfResult<&[u8]>)
+    where I: Interpretable + ?Sized
+{
+    let actual_bytes = program.interpret_memory(None, input);
+    let actual = actual_bytes.map(|bytes| str::from_utf8(&bytes).unwrap().to_owned());
+    let expected = output.map(|bytes| str::from_utf8(bytes).unwrap().to_owned());
 
     assert_eq!(actual, expected);
+
 }
