8fd8dad47d1c2847fbd0a0af5946419a6a1cb32d
Jesse A. Tov <jesse.tov@gmail.com>
2017-05-26 15:33:31 -0600

    Update documentation

--- bf/test_helpers/sidebar-items.js
+++ bf/test_helpers/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"constant":[["FACTOR_SRC",""],["HELLO_WORLD_SRC",""]],"fn":[["assert_interpret",""],["assert_interpret_result",""]]});
+initSidebarItems({"constant":[["FACTOR_SRC","Source of the factoring program from [`../bf/factor.bf`]."],["HELLO_WORLD_SRC","Source of a hello program."]],"fn":[["assert_interpret","Interprets `program`, giving it input `input`, and asserting that its output is `output`."],["assert_interpret_result","Interprets `program`, giving it input `input`, and asserting that its result is `output`."]]});
