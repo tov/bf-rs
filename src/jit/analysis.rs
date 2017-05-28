05c0134b640318d08b8e9640fdced2490ef1fbbc
Jesse A. Tov <jesse.tov@gmail.com>
2017-05-28 08:56:51 -0500

    Performs loop balance analysis.

--- src/jit/analysis.rs
+++ src/jit/analysis.rs
@@ -6,14 +6,45 @@ use peephole::{Statement, Program};
 #[derive(Copy, Clone, Debug, Eq, PartialEq)]
 /// The net movement of a loop.
 enum LoopBalance {
+    /// The exact movement of one iteration.
+    Exact(isize),
     /// May move right but not left.
-    NoLeft,
+    RightOnly,
     /// May move left but not right.
-    NoRight,
-    /// No net movement.
-    Balanced,
+    LeftOnly,
     /// Net movement may be either direction.
     Unknown,
+}
+
+impl LoopBalance {
+    /// Is the loop body exactly balanced between right and left?
+    fn is_balanced(self) -> bool {
+        self == LoopBalance::Exact(0)
+    }
+
+    /// Does the loop move net right (if at all)?
+    fn is_right_only(self) -> bool {
+        use self::LoopBalance::*;
+
+        match self {
+            Exact(disp) => disp >= 0,
+            RightOnly   => true,
+            LeftOnly    => false,
+            Unknown     => false,
+        }
+    }
+
+    /// Does the loop move net left (if at all)?
+    fn is_left_only(self) -> bool {
+        use self::LoopBalance::*;
+
+        match self {
+            Exact(disp) => disp <= 0,
+            RightOnly   => false,
+            LeftOnly    => true,
+            Unknown     => false,
+        }
+    }
 }
 
 /// An index to a loop.
@@ -25,7 +56,7 @@ struct LoopIndex(usize);
 impl LoopIndex {
     /// Gets the loop index from a boxed loop.
     fn from_loop_body(body: &Box<[Statement]>) -> Self {
-        LoopIndex(&body[0] as *const Statement as usize)
+        LoopIndex(body.as_ptr() as usize)
     }
 }
 
@@ -48,11 +79,68 @@ impl AbstractInterpreter {
     ///
     /// The interpreter initially analyzes the program for loop balances.
     pub fn new(program: &Program) -> Self {
-        AbstractInterpreter {
+        let mut result = AbstractInterpreter {
             left_mark: 0,
             right_mark: 0,
             loop_balances: HashMap::new(),
+        };
+
+        result.analyze(program);
+
+        result
+    }
+
+    fn analyze(&mut self, statements: &[Statement]) -> LoopBalance {
+        use peephole::Statement::*;
+        use common::Instruction::*;
+        use self::LoopBalance::*;
+
+        let mut net: LoopBalance = Exact(0);
+
+        for statement in statements {
+            match *statement {
+                Instr(Right(count)) => net = match net {
+                    Exact(disp)         => Exact(disp + count as isize),
+                    RightOnly           => RightOnly,
+                    _                   => Unknown,
+                },
+
+                Instr(Left(count)) => net = match net {
+                    Exact(disp)         => Exact(disp - count as isize),
+                    LeftOnly            => LeftOnly,
+                    _                   => Unknown,
+                },
+
+                Instr(Add(_)) | Instr(In) | Instr(Out) => (),
+
+                Instr(JumpZero(_)) | Instr(JumpNotZero(_)) =>
+                    panic!("unexpected jump instruction"),
+
+                Instr(SetZero) | Instr(OffsetAddRight(_)) | Instr(OffsetAddLeft(_)) => (),
+
+                Instr(FindZeroRight(_)) =>
+                    net = if net.is_right_only() {RightOnly} else {Unknown},
+
+                Instr(FindZeroLeft(_)) =>
+                    net = if net.is_left_only() {LeftOnly} else {Unknown},
+
+                Loop(ref body) => {
+                    let index = LoopIndex::from_loop_body(body);
+                    let body  = self.analyze(body);
+
+                    self.loop_balances.insert(index, body);
+
+                    net = match net {
+                        Exact(disp) if body.is_balanced()                   => Exact(disp),
+                        _ if net.is_right_only() && body.is_right_only()    => RightOnly,
+                        _ if net.is_left_only() && body.is_left_only()      => LeftOnly,
+                        _                                                   => Unknown,
+                    }
+                }
+            }
         }
+
+        net
     }
 
     /// Moves the pointer the given distance to the left.
