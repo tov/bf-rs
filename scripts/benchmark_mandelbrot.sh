c7f9fbad561294459b4b58b0c7b3d50218afd93d
Jesse A. Tov <jesse.tov@gmail.com>
2017-05-28 12:09:49 -0500

    Added --flat to benchmark; removed optinterp and optinterp2.

--- scripts/benchmark_mandelbrot.sh
+++ scripts/benchmark_mandelbrot.sh
@@ -13,11 +13,12 @@ bench () {
     echo
 }
 
-bench "Peephole AST" target/release/bfi --peep bf/mandelbrot.bf
-bench "Native JIT" target/release/bfi --jit bf/mandelbrot.bf
-bench "Native JIT (unchecked)" target/release/bfi --jit -u bf/mandelbrot.bf
-bench "Bendersky's optinterp" cpp/optinterp bf/mandelbrot.bf
-bench "Bendersky's optinterp2" cpp/optinterp2 bf/mandelbrot.bf
+bench "bfi peephole AST" target/release/bfi --peep bf/mandelbrot.bf
+bench "bfi bytecode" target/release/bfi --flat bf/mandelbrot.bf
+bench "bfi native JIT" target/release/bfi --jit bf/mandelbrot.bf
+bench "bfi native JIT (unchecked)" target/release/bfi --jit -u bf/mandelbrot.bf
+# bench "Bendersky's optinterp" cpp/optinterp bf/mandelbrot.bf
+# bench "Bendersky's optinterp2" cpp/optinterp2 bf/mandelbrot.bf
 bench "Bendersky's optinterp3" cpp/optinterp3 bf/mandelbrot.bf
 bench "Bendersky's optasmjit" cpp/optasmjit bf/mandelbrot.bf
 
