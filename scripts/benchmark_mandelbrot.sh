488a8d12411bda84047a238adfc4134b6b391acc
Jesse A. Tov <jesse.tov@gmail.com>
2017-05-28 11:00:12 -0500

    Added Mandelbrot bench for Travis CI.

---
+++ scripts/benchmark_mandelbrot.sh
@@ -0,0 +1,11 @@
+#/bin/sh -ex
+
+( cd cpp; make )
+
+cargo build --release --features=jit
+
+time cpp/optinterp bf/mandelbrot.bf >/dev/null
+time cpp/optinterp2 bf/mandelbrot.bf >/dev/null
+time target/release/bfi --peep bf/mandelbrot.bf >/dev/null
+time target/release/bfi --jit bf/mandelbrot.bf >/dev/null
+time target/release/bfi --jit -u bf/mandelbrot.bf >/dev/null
