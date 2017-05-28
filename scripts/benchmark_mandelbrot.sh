d6937c2d207b382706506407891380043767399c
Jesse A. Tov <jesse.tov@gmail.com>
2017-05-28 12:02:05 -0500

    Added Bendersky's optasmjit to benchmark script.

--- scripts/benchmark_mandelbrot.sh
+++ scripts/benchmark_mandelbrot.sh
@@ -19,5 +19,6 @@ bench "Native JIT (unchecked)" target/release/bfi --jit -u bf/mandelbrot.bf
 bench "Bendersky's optinterp" cpp/optinterp bf/mandelbrot.bf
 bench "Bendersky's optinterp2" cpp/optinterp2 bf/mandelbrot.bf
 bench "Bendersky's optinterp3" cpp/optinterp3 bf/mandelbrot.bf
+bench "Bendersky's optasmjit" cpp/optasmjit bf/mandelbrot.bf
 
 exit 0
