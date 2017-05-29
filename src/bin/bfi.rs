ba6473e257c84e0974f9a4be61a1055e3f8154af
Jesse A. Tov <jesse.tov@gmail.com>
2017-05-29 13:07:46 -0500

    Version: 0.4.1

--- src/bin/bfi.rs
+++ src/bin/bfi.rs
@@ -36,7 +36,7 @@ use clap::{Arg, App};
 use bf::ast;
 use bf::traits::*;
 
-const VERSION: &'static str = "0.4.0";
+const VERSION: &'static str = "0.4.1";
 
 #[derive(Debug, Clone)]
 struct Options {
