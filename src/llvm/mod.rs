337991325517d2732b5cc647aee9bae18910b550
Jesse A. Tov <jesse.tov@gmail.com>
2017-06-03 10:02:58 -0500

    Added LLVM explanation.

--- src/llvm/mod.rs
+++ src/llvm/mod.rs
@@ -1,4 +1,8 @@
 //! JIT compiler for Brainfuck based on LLVM.
+//!
+//! Enabled with `--features=llvm`. This currently doesn't link on some platforms due to trouble
+//! with libffi. And when it does link, it appears to use the LLVM bitcode interpreter rather
+//! than MCJIT, and consequently is very slow.
 
 mod wrapper;
 mod compiler;
